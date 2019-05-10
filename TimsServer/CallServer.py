fb344632a52ac5b8e7ab74f3b94a1320bae45d33
David Daghelian <DavidDaghelian@oc7511546407.ibm.com>
2019-05-10 12:37:26 -0400

    Basic Rest API server completed The call server gets rest api calls get and put. Put sets the server_data.dat contents to the given data and sends it back to the client. Get gets the contents of server_data.dat and sends it back to the client.

---
+++ TimsServer/CallServer.py
@@ -0,0 +1,33 @@
+from flask import Flask
+from flask_restful import Api, Resource, reqparse
+
+app = Flask(__name__)
+api = Api(app)
+
+server_file = "server_data.dat"
+
+# Create a URL route in the application for "/"
+@app.route('/')
+
+
+class User(Resource):
+    def get(self, data):
+        server_data = open(server_file, 'r')  
+        data = server_data.read()
+        server_data.close()
+        return data
+    
+    def put(self, data):
+        server_data = open(server_file, 'w')  
+        server_data.write(data)
+        server_data.close()
+        return data
+    
+    def post(self, data):
+        pass    
+    
+    def delete(self, data):
+        pass
+
+api.add_resource(User, "/<string:data>")
+app.run(debug=True)
