dbf1ea39186857c3db36704fbfa1a424a9757ed4
David Daghelian <DavidDaghelian@oc7511546407.ibm.com>
2019-05-13 10:48:15 -0400

    Cleaned up the call server and the main server Removed some code that is not needed and added comments throughout the code.

--- TimsServer/CallServer.py
+++ TimsServer/CallServer.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_restful import Api, Resource, reqparse
 from flask import request
 
+#Defining The API Application
 app = Flask(__name__)
 api = Api(app)
 
@@ -10,30 +11,29 @@ server_file = "server_data.dat"
 # Create a URL route in the application for "/"
 @app.route('/')
 
-
+#The class that contains all of the rest api calls
+#If a function is missing then it is qualified as not allowed for the url
 class User(Resource):
+    #Run when recieve a get rest api call
+    #Reads off the data from the server file
     def get(self):
         server_data = open(server_file, 'r')  
         data = server_data.read()
         server_data.close()
         return data
     
+    #Run when recieve a put rest api call
+    #Writes the call arguments to the server file
     def put(self):
+        #Grabs the arguments from the call
         args = request.args
-        #print("Stuff")
-        #print(args)
         data = args["data"]
+        #Writes the arguments to the server file
         server_data = open(server_file, 'w')  
         server_data.write(data)
         server_data.close()
         return data
-    
-    def post(self):
-        pass    
-    
-    def delete(self):
-        pass
 
-#api.add_resource(User, "/<string:data>")
+#Runs the rest api application
 api.add_resource(User, "/data")
 app.run(debug=True)
