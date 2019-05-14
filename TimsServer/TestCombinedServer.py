a996e759bfbf517317dc080e52f9ebcb2f353bd6
David Daghelian <DavidDaghelian@oc7511546407.ibm.com>
2019-05-14 11:13:01 -0400

    Upgraded the testing program The TestCombinedServer.py now has functionality to test the get and the post commands for the combined server program.

--- TimsServer/TestCombinedServer.py
+++ TimsServer/TestCombinedServer.py
@@ -2,11 +2,17 @@ import json
 import requests
 import datetime
 
-api_url = "http://127.0.0.1:5000/data"
-img = "test.jpg"
-file = open(img, "rb")
-img_time = datetime.datetime.now()
+api_url = "http://drv-ctp6.canlab.ibm.com/:5000/data"
+get = False
 
-req = requests.post(url = api_url, files={"files": (img, file)}, verify=False)
+if (get):
+    req = requests.get(url = api_url)
+else:    
+    img = "test.jpg"
+    file = open(img, "rb")
+    img_time = datetime.datetime.now()
+    req = requests.post(url = api_url, files={"files": (img, file)}, verify=False)
+    
+print(req)
 response = json.loads(req.text)
 print(response)
