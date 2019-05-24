43f02bff419db1b06a963bb1f905ec0e9bdecf8a
David Daghelian <DavidDaghelian@oc7511546407.ibm.com>
2019-05-24 09:52:24 -0400

    Removed spaces in the server storage titles and added new titles

--- TimsServer/RestartServerStorage.py
+++ TimsServer/RestartServerStorage.py
@@ -1,6 +1,6 @@
 #Very basic program but it does the job since
 print("Resetting the server storage file")
 f = open("server_storage.csv", "w")
-f.write("Date,Day Of The Week,Temperature,Weather,Time,Number Of People,Longitude,Latitude")
+f.write("Date,DayOfThe Week,Temperature,Weather,Time,NumberOfPeople,Longitude,Latitude")
 f.close
 print("Done Reset")
