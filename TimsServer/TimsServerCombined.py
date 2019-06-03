5a377ce39bab797c801bd2b0f5102dec0e6137f7
David Daghelian <david.daghelian@ibm.com>
2019-06-03 16:08:48 -0400

    Reversed a previous commit making the kill signal be SIGINT rather than SIGKILL

--- TimsServer/TimsServerCombined.py
+++ TimsServer/TimsServerCombined.py
@@ -87,7 +87,7 @@ class User(Resource):
         #If a get request is made when it is 6 pm, the server shuts down
         #This is done so after 6 pm, the server shuts down
         if request.args.get("kill") == "True" and auto_reset:
-            os.kill(os.getpid(), signal.SIGKILL)
+            os.kill(os.getpid(), signal.SIGINT)
         #Get the long and lat of the client
         lat, long = get_latlng(request, location_precision)
         
