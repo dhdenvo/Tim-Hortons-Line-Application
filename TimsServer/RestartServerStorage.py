1a560fd3d7703e70fe32f723eb28f84fde1fc803
David Daghelian <david.daghelian@ibm.com>
2019-06-07 11:13:09 -0400

    Made the restart server storage program delete all graphs (Other than NoGraph.png)

--- TimsServer/RestartServerStorage.py
+++ TimsServer/RestartServerStorage.py
@@ -1,6 +1,14 @@
+import os
+
 #Very basic program but it does the job since
 print("Resetting the server storage file")
 f = open("server_storage.csv", "w")
 f.write("Date,DayOfTheWeek,Time,NumberOfPeople,Latitude,Longitude")
 f.close
 print("Done Reset")
+
+for graph in os.listdir("./Graphs/"):
+    print(graph)
+    if os.path.isfile("./Graphs/" + graph) and graph != "NoGraph.png":
+        os.remove("./Graphs/" + graph)
+        print("File Removed!")
